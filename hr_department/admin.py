166a1fe02d1c7e861b66b960815dd061cb7103a7
Maydash <maydashdowletow@gmail.com>
2023-07-12 12:27:49 +0500

    add grapelli for admin panel

--- hr_department/admin.py
+++ hr_department/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 from .models import *
 
+
+@admin.register(PersonInformation)
+class PersonInformationAdmin(admin.ModelAdmin):
+    search_fields = (
+        'first_name',
+        'last_name',
+    )
+    list_filter = ('id', 'height', 'weight',)
+    list_display_links = ('last_name', 'first_name',)
+    list_display = ('id',
+                    'last_name',
+                    'first_name',
+                    'date_of_birthday',
+                    'nationality',
+                    'address',
+                    'foreign_languages',
+                    'nationality',
+                    'height',
+                    'weight',
+                    'phone_number',
+                    'person_education',
+                    'work_experience',
+                    'car_info',
+                    )
+
+
 admin.site.register(Logos)
-admin.site.register(PersonInformation)
+# admin.site.register(PersonInformation)
 admin.site.register(PersonEducation)
 admin.site.register(WasHeAbroad)
 admin.site.register(StateAwards)
